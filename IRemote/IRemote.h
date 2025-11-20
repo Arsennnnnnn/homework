072ae0e27495e55cb47143507d2627f83390ad91
Arsennnnnnn <arsenavetisyann20@gmail.com>
2025-11-20 22:46:52 +0400

    Remote commit

--- IRemote/IRemote.h
+++ IRemote/IRemote.h
@@ -9,7 +9,7 @@ public:
     virtual void TurnOf() = 0;
     virtual void increase() = 0; //for tv remote increase volume, for cooler increase the temperature etc…
     virtual void decrease() = 0; //just like the increase this one for decreasing.
-    virtual void IsOn() = 0;
+    virtual bool IsOn() = 0;
 };
 
 #endif //IREMOTE_H
